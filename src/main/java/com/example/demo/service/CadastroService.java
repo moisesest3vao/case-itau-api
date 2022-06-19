b0188faff82bb74d73fdfdb127ea8ad65c6cea99
Moisés Estevão <moises.silva@soluarq.com.br>
2022-06-19 00:34:10 -0300

    refactor

--- src/main/java/com/example/demo/service/CadastroService.java
+++ src/main/java/com/example/demo/service/CadastroService.java
@@ -17,57 +17,55 @@ public class CadastroService {
 
     public List<Colaborador> cadastrarColaboradores() {
         List<String> nomesDosColaboradores = this.cadastroRepository.getAll();
-        List<Colaborador> colaboradores = nomesDosColaboradores.stream().map(this::criarLogin).toList();
+        List<Colaborador> colaboradores = nomesDosColaboradores.stream().map(this::gerarLogin).toList();
 
-        //resetando o histórico para cada chamada
+        /*
+           Neste exemplo utilizei chamadas iguais repetidamente. Por isso se faz necessário o reset do
+           'ColaboradorUtil.históricoDeLogins' na linha 27 para que seja possível gerar o cadastro com os nomes iguais
+           na próxima requisição. Em produção isso não seria necessário.
+         */
         ColaboradorUtil.historicoLogins = new ArrayList<>();
+
         return colaboradores;
     }
 
-    public Colaborador criarLogin(String nomeCompleto) {
+    public Colaborador gerarLogin(String nomeCompleto) {
         String nomeSemElementos = ColaboradorUtil.removeElementosDeLigacao(nomeCompleto);
-        String[] nomes = nomeSemElementos.split(" ");
-        String login = this.criaIdentificador(nomes);
+        String[] nomeESobrenomes = nomeSemElementos.split(" ");
+        String login = this.montaLogin(nomeESobrenomes);
 
         return new Colaborador(nomeCompleto, login);
     }
 
-    public String criaIdentificador(String[] nomes) {
-        String login = this.montaLogin(nomes);
-
-        ColaboradorUtil.historicoLogins.add(login);
-        return login;
-    }
-
-    private String montaLogin(String[] nomes) {
+    private String montaLogin(String[] nomeESobrenomes) {
         String parte1 = null;
         String parte2 = null;
 
         boolean invalidaLogin = true;
-        int quantidadeDeNomes = nomes.length;
+        int quantidadeDeNomes = nomeESobrenomes.length;
         int contador = 0;
 
         while (invalidaLogin) {
-            if (contador > 3000) {
+            if (contador > 9999) {
                 throw new RuntimeException("Não é possível gerar um usuário dinamicamente com este nome");
             }
 
             Random random = new Random();
-            int aux = random.nextInt(((quantidadeDeNomes - 1)) + 1);
-            int aux2 = random.nextInt(((quantidadeDeNomes - 1)) + 1);
+            int indexNome1 = random.nextInt(((quantidadeDeNomes - 1)) + 1);
+            int indexNome2 = random.nextInt(((quantidadeDeNomes - 1)) + 1);
 
-            if (nomes[aux].length() >= 4) {
-                parte1 = nomes[aux].substring(0, 4);
-                if (nomes[aux2].length() >= 3) {
-                    parte2 = nomes[aux2].substring(0, 3);
-                }
-            }
+            parte1 = nomeESobrenomes[indexNome1].length() >= 4 ? nomeESobrenomes[indexNome1].substring(0, 4) : null;
+            parte2 = nomeESobrenomes[indexNome2].length() >= 3 ? nomeESobrenomes[indexNome2].substring(0, 3) : null;
 
             invalidaLogin = ColaboradorUtil.invalidaLogin(parte1, parte2);
+
             contador++;
         }
 
-        return parte1 + parte2;
+        String login = parte1+parte2;
+
+        ColaboradorUtil.historicoLogins.add(login);
+        return login;
     }
 
 
